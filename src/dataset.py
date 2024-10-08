ec9761472c2640398fe2dc292f9bd35fc0a4ade5
MEDIENE <yanis.mediene@gmail.com>
2024-10-08 19:15:24 +0200

    setup

---
+++ src/dataset.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# 2. Creating a DataFrame
+data = {
+    "Name": ["Alice", "Bob", "Charlie", "David"],
+    "Age": [24, 27, 22, 32],
+    "City": ["New York", "Los Angeles", "Chicago", "Houston"],
+}
+df = pd.DataFrame(data)
